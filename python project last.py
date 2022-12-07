bfbf31a6d2e64e7e5e7d6a176fdf8907e8010f64
harinanis <118800249+harinanis@users.noreply.github.com>
2022-12-07 18:50:08 +0530

    Update python project last.py

--- python project last.py
+++ python project last.py
@@ -7,6 +7,7 @@ player_score = 0
 while True:
     player = input("Rock, Paper or  Scissors?").capitalize()
     ## Conditions of Rock,Paper and Scissors
+    #
     if player == computer:
         print("Tie!")
     elif player == "Rock":
